ce19fb8faad3d94f6f579d2fb765685cf6d2e402
AnB801 <antony.bondarev@gmail.com>
2023-05-26 14:30:19 +0200

    All request are corrected, working and in line with the assignment

--- src/backend/app.js
+++ src/backend/app.js
@@ -21,73 +21,89 @@ app.use(express.json())
 app.use(cors())
 
 //localhost:5000/api/meals
-router.use('/Meal', mealsRouter)
+router.use('/meals', mealsRouter)
 router.use('/Reservations', reservationsRouter)
-router.use('/Reviews', reviewsRouter)
+// router.use('/Reviews', reviewsRouter)
 
 //--Homework week1--
 
 // like Ahmed say ))
 const knex = require('./database')
 
-app.get('/all-meals', (req, res) => {
-  knex
-    .raw('SELECT * FROM Meal ORDER BY id')
-    .then((rows) => {
-      const jsonResponce = JSON.stringify(rows)
-      res.send(jsonResponce)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+//This query gives me additional fields in the data array that I don't really understand
+// app.get('/all-meals', (req, res) => {
+//   knex
+//     .raw('SELECT * FROM Meal ')
+//     .then((rows) => {
+//       const jsonResponce = JSON.stringify(rows)
+//       res.send(jsonResponce)
+//     })
+//     .catch((error) => {
+//       console.error(error)
+//     })
+// })
+
+// This query works correctly.Rewrite all queries according to this
+
+app.get('/all-meals', async (req, res) => {
+  try {
+    const allMeals = await knex('Meal').orderBy('id', 'desc')
+    res.json(allMeals)
+  } catch (error) {
+    throw error
+  }
 })
 
-app.get('/future-meals', (req, res) => {
-  knex
-    .raw("SELECT * FROM Meal WHERE 'when_created'<'01-01-2023'")
-    .then((rows) => {
-      const jsonResponce = JSON.stringify(rows)
-      res.send(jsonResponce)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+app.get('/future-meals', async (req, res) => {
+  try {
+    const futureMeals = await knex('Meal').where(
+      'created_date',
+      '>',
+      '2023-02-02'
+    )
+    res.json(futureMeals)
+  } catch (error) {
+    throw error
+  }
 })
 
-app.get('/past-meals', (req, res) => {
-  knex
-    .raw("SELECT * FROM Meal WHERE 'when_created'>'01-01-2023'")
-    .then((rows) => {
-      const jsonResponce = JSON.stringify(rows)
-      res.send(jsonResponce)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+app.get('/past-meals', async (req, res) => {
+  try {
+    const futureMeals = await knex('Meal').where(
+      'created_date',
+      '<',
+      '2023-02-02'
+    )
+    res.json(futureMeals)
+  } catch (error) {
+    throw error
+  }
 })
 
-app.get('/first-meal', (req, res) => {
-  knex
-    .raw('SELECT FIRST(`id`) FROM Meal')
-    .then((rows) => {
-      const jsonResponce = JSON.stringify(rows)
-      res.send(jsonResponce)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+app.get('/first-meal', async (req, res) => {
+  try {
+    const firstMeal = await knex('Meal').orderBy('id').first()
+    if (!firstMeal) {
+      res.status(404)
+    } else {
+      res.json(firstMeal)
+    }
+  } catch (error) {
+    throw error
+  }
 })
 
-app.get('/last-meal', (req, res) => {
-  knex
-    .raw('SELECT MAX(`id`) FROM Meal')
-    .then((rows) => {
-      const jsonResponce = JSON.stringify(rows)
-      res.send(jsonResponce)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+app.get('/last-meal', async (req, res) => {
+  try {
+    const lastMeal = await knex('Meal').orderBy('id', 'desc').first()
+    if (!lastMeal) {
+      res.status(404)
+    } else {
+      res.json(lastMeal)
+    }
+  } catch (error) {
+    throw error
+  }
 })
 
 //-- end
