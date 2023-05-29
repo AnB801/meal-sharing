d23129ceefd4ebc75754fe72b7915e597e0b497b
AnB801 <antony.bondarev@gmail.com>
2023-05-29 15:48:16 +0200

    add reservation rules

--- src/backend/api/reservations.js
+++ src/backend/api/reservations.js
@@ -4,12 +4,36 @@ const knex = require('../database')
 const reservationRouter = express.Router()
 
 // all
+
 reservationRouter.get('/', async (req, res) => {
   try {
-    const reservations = await knex('Reservation').select('*')
-    if (!reservations.length) {
-      response.status(404).json({ error: 'That Reservation doesn’t exist.' })
+    const availableReservations = req.query.availableReservations
+    let reservations
+
+    if (availableReservations) {
+      const mealIdsWithReservations = await knex('Reservations').pluck(
+        'meal_id'
+      )
+      if (availableReservations === 'true') {
+        reservations = await knex('Reservation')
+          .join('Meal', 'Reservation.meal_id', 'Meal.id')
+          .select('Reservation.*', 'Meal.*')
+          .whereIn('Meal.id', mealIdsWithReservations)
+      } else if (availableReservations === 'false') {
+        reservations = await knex('Reservation')
+          .join('Meal', 'Reservation.meal_id', 'Meal.id')
+          .select('Reservation.*', 'Meal.*')
+          .whereNotIn('Meal.id', mealIdsWithReservations)
+      }
+    } else {
+      reservations = await knex('Reservation').select('*')
     }
+
+    if (!reservations.length) {
+      res.status(404).json({ error: 'That Reservation doesn’t exist.' })
+      return
+    }
+
     res.json(reservations)
   } catch (error) {
     res.status(500).json({ error: 'Error retrieving reservations' })
