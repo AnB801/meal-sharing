d511922e7332aa825d4983bd5bb71a9da7efa008
AnB801 <antony.bondarev@gmail.com>
2023-05-29 15:59:42 +0200

    add error catch

--- src/backend/api/reviews.js
+++ src/backend/api/reviews.js
@@ -1,26 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../database')
+const reviewRouter = express.Router()
 
-router.get('/api/reviews', async (req, res) => {
-  const allReviews = await knex('reviews').select('*')
-  res.json(allReviews)
+reviewRouter.get('/api/reviews', async (req, res) => {
+  try {
+    const allReviews = await knex('Reviews').select('*')
+    res.json(allReviews)
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving reviews' })
+  }
 })
 
-router.get('/api/meals/:meal_id/reviews', async (req, res) => {
-  const reviews = await knex('reviews')
-    .select('*')
-    .where({ meal_id: req.params.meal_id })
-  res.json(reviews)
+reviewRouter.get('/api/meals/:meal_id/reviews', async (req, res) => {
+  try {
+    const reviews = await knex('reviews')
+      .select('*')
+      .where({ meal_id: req.params.meal_id })
+    res.json(reviews)
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving reviews' })
+  }
 })
 
-router.post('/api/reviews', async (req, res) => {
+reviewRouter.post('/api/reviews', async (req, res) => {
   const newReview = req.body
   await knex('reviews').insert(newReview)
   res.status(201).json(newReview)
 })
 
-router.get('/api/reviews/:id', async (req, res) => {
+reviewRouter.get('/api/reviews/:id', async (req, res) => {
   const review = await knex('reviews').select('*').where({ id: req.params.id })
   if (review.length === 0) {
     res.status(404).send({ message: 'Review not found' })
@@ -29,15 +38,15 @@ router.get('/api/reviews/:id', async (req, res) => {
   }
 })
 
-router.put('/api/reviews/:id', async (req, res) => {
+reviewRouter.put('/api/reviews/:id', async (req, res) => {
   const updatedReview = req.body
   await knex('reviews').where({ id: req.params.id }).update(updatedReview)
   res.json(updatedReview)
 })
 
-router.delete('/api/reviews/:id', async (req, res) => {
+reviewRouter.delete('/api/reviews/:id', async (req, res) => {
   await knex('reviews').where({ id: req.params.id }).del()
   res.status(200).send({ message: 'Review deleted' })
 })
 
-module.exports = router
+module.exports = reviewRouter
